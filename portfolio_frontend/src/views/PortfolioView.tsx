e7065ae03d5c7d73311b10f17c12606a108dcd95
Sherwin <limosnerosherwin@gmail.com>
2026-03-31 21:36:50 +0800

    Final project: smart contract + frontend integration

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0x790f953ca80b53e99e4fc179fb92cf6d331899ba24d5ed5f3b9a839f09317ab7";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
